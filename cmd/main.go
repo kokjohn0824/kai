5c656c1b766dbefce4cb45d8278e8d65f1472383
Alex Chang <alexchang0824@gmail.com>
2024-05-17 17:47:59 +0800

    you can now add multiple arguments

--- cmd/main.go
+++ cmd/main.go
@@ -11,15 +11,16 @@ type Context struct {
 
 func main() {
 	if len(os.Args) < 2 {
+		//TODO: update usage message
 		fmt.Println("usage: create go project")
 		os.Exit(0)
 	}
 
-	projectName := os.Args[1]
-	err := createProject(projectName)
-
-	if err != nil {
-		fmt.Println(err)
+	for _, projectName := range os.Args[1:] {
+		err := createProject(projectName)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	fmt.Println("project created, enjoy!")
